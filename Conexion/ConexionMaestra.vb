91a1fa47d80c23cc8e251e41e93fd9bb09d76339
BrandonVielmas <75179907+BrandonVielmas@users.noreply.github.com>
2021-11-21 19:35:40 -0600

    Update ConexionMaestra.vb

--- Conexion/ConexionMaestra.vb
+++ Conexion/ConexionMaestra.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.SqlClient
 Module ConexionMaestra
     'Variable con la conexion sql
-    Public conexion As New SqlConnection("Data Source = DESKTOP-1LA75V6; initial catalog = BASECOLEGIO; integrated security = true")
+    Public conexion As New SqlConnection("Data Source = *********; initial catalog = BASECOLEGIO; integrated security = true")
     'Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
     'funcion que la abre solo cuando su estado sea cero
     Sub abrir()
